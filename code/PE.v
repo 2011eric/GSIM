48ba5aa3a59b0daef54fb1e2ccb0f9383a448561
2011eric <201165r1c@gmail.com>
2024-04-22 19:08:12 +0800

    Implement divider part in PE

--- code/PE.v
+++ code/PE.v
@@ -1,13 +1,16 @@
 /*
- *  out = (b + (in_1 + in_2) - 6 * (in_3 + in_4) + 13 * (in_5 + in_6)) / 20
- *
+ * This module is used to calculate the following equation:
+ *      out = (b + (in_1 + in_2) - 6 * (in_3 + in_4) + 13 * (in_5 + in_6)) / 20
+ * The total delay is 2 + divider_stage
+ * In current implementation, it will be 5 cycles
  */
 module PE (input clk, 
            input reset,
-           input signed [33:0] in_1, in_2, in_3, in_4, in_5, in_6,
+           input signed [31:0] in_1, in_2, in_3, in_4, in_5, in_6,
            input signed [15:0] b,
-           output reg [37:0] out);
-    
+           output [31:0] out);
+
+reg signed [15:0] b_r, b_w;
 reg signed [32:0] s1_adder [0:2];
 reg signed [35:0] s1_mul6;
 reg signed [36:0] s1_mul13;
@@ -19,36 +22,84 @@ reg signed [36:0] s1_reg2_r, s1_reg2_w;
 reg signed [37:0] s2_adder;
 reg signed [37:0] s2_reg0_r, s2_reg0_w;
 
+wire signed [34:0] div_out;
 always @(posedge clk or posedge reset) begin
     if (reset) begin
         s1_reg0_r <= 0;
         s1_reg1_r <= 0;
         s1_reg2_r <= 0;
         s2_reg0_r <= 0;
+        b_r <= 0;
     end else begin
         s1_reg0_r <= s1_reg0_w;
         s1_reg1_r <= s1_reg1_w;
         s1_reg2_r <= s1_reg2_w;
         s2_reg0_r <= s2_reg0_w;
+        b_r <= b_w;
     end
 end
 
 always @(*) begin:stage1
+    b_w = b;
     s1_adder[0] = in_1 + in_2;
     s1_adder[1] = in_3 + in_4;
     s1_adder[2] = in_5 + in_6;
     s1_mul6 = $signed(s1_adder[1] << 1) + $signed(s1_adder[1] << 2);
-    s1_mul13 = $signed(s1_adder[2] << 3) + $signed(s1_adder[2] << 2) + $signed(s1_adder[2] << 1);
+    s1_mul13 = $signed(s1_adder[2] << 3) + $signed(s1_adder[2] << 2) + s1_adder[2];
     s1_reg0_w = s1_adder[0];
     s1_reg1_w = s1_mul6;
     s1_reg2_w = s1_mul13;
 end
 
 always @(*) begin:stage2
-    s2_adder = b + s1_reg0_r - s1_reg1_r + s1_reg2_r;
+    s2_adder = b_r + s1_reg0_r - s1_reg1_r + s1_reg2_r;
     s2_reg0_w = s2_adder;
 end
 
-assign out = s2_reg0_r;
+assign out = div_out;
 
+
+Divider #(38, 3) div (
+    .clk(clk),
+    .reset(reset),
+    .in(s2_reg0_r),
+    .out(div_out)
+);
+endmodule
+
+/* 
+ * This module is used to divide the input by 20, the number of pipeline stages 
+ * is configurable via the parameter STAGE
+ * The number of stages affect the accuracy of the division
+ */
+module Divider #(parameter WIDTH = 38,
+                 parameter STAGE = 3)(
+                 input clk, 
+                 input reset,
+                 input signed [WIDTH-1:0] in,
+                 output signed [WIDTH-4:0] out);
+
+integer i;
+reg signed [WIDTH-1+3:0] x_r [0:STAGE-1], x_w [0:STAGE-1];
+assign out = x_r[STAGE-1];
+
+always @(*) begin
+    /* First, calculate 3/2^6 * input */
+    x_w[0] = (in + (in << 1)) >>> 6;
+    for (i = 1; i < STAGE; i = i + 1) begin
+        x_w[i] = x_r[i-1] + (x_r[i-1] >>> (4*i));
+    end
+end
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        for (i = 0; i < STAGE; i = i + 1) begin
+            x_r[i] <= 0;
+        end
+    end else begin
+        for (i = 0; i < STAGE; i = i + 1) begin
+            x_r[i] <= x_w[i];
+        end
+    end
+end
 endmodule
--- code/tb_pe.v
+++ code/tb_pe.v
@@ -6,11 +6,11 @@
 module testbench;
 
 reg clk, reset;
-reg [33:0] in_1, in_2, in_3, in_4, in_5, in_6;
-reg [15:0] b;
-wire [37:0] out;
+reg signed [31:0] in_1, in_2, in_3, in_4, in_5, in_6;
+reg signed [15:0] b;
+wire signed [37:0] out;
 
-reg [15:0] pat_mem [0:15];
+reg signed [15:0] pat_mem [0:15];
 
 always #(`CYCLE/2) clk = ~clk;
 
@@ -31,15 +31,25 @@ initial begin
     clk = 0;
     @(negedge clk) reset = 1;
     @(negedge clk) reset = 0;
-    in_1 = pat_mem[3]; // x4
+    in_1 = pat_mem[3]/32'sd20 ; // x4
     in_2 = 0;
-    in_3 = pat_mem[2]; // x3
+    in_3 = pat_mem[2]/32'sd20; // x3
     in_4 = 0;
-    in_5 = pat_mem[1]; // x2
+    in_5 = pat_mem[1]/32'sd20; // x2
     in_6 = 0;
-    b = pat_mem[0]/20; // x1
+    b = pat_mem[0]; // x1
 
-    repeat(3) @(posedge clk);
+    //FIXME Clarify the dependency, since the current input seems incorrect
+    @(negedge clk);
+    in_1 = pat_mem[4]/32'sd20;
+    in_2 = 0;
+    in_3 = pat_mem[3]/32'sd20;
+    in_4 = 0;
+    in_5 = pat_mem[2]/32'sd20;
+    in_6 = pat_mem[0]/32'sd20;
+    b = pat_mem[1]; // x2
+
+    repeat(11) @(posedge clk);
     $finish;
 end
 
